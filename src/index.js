ed3800c1640886f6424e38390c7f6e5e4355a951
Alexander <MrRandomgamerdkHD@gmail.com>
2019-11-27 15:09:18 +0100

    API done

---
+++ src/index.js
@@ -0,0 +1,27 @@
+const { prisma } = require('./generated/prisma-client')
+const { GraphQLServer } = require('graphql-yoga')
+const Query = require('./resolvers/Query')
+const Mutation = require('./resolvers/Mutation')
+const Subscription = require('./resolvers/Subscription')
+const User = require('./resolvers/User')
+const Link = require('./resolvers/Link')
+const Vote = require('./resolvers/Vote')
+
+const resolvers = {
+  Query,
+  Mutation,
+  Subscription,
+  User,
+  Link,
+  Vote
+}
+
+const server = new GraphQLServer({
+  typeDefs: './src/schema.graphql',
+  resolvers,
+  context: request => {
+    return { ...request, prisma }
+  }
+})
+
+server.start(() => console.log('Server running on port 4000'))
